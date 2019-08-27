99fdcef5bdf7cc44651b2f390ac4ef212d3ac00c
Andrew Seba <sebaturtle@gmail.com>
2019-08-27 15:57:59 -0500

    Main Menu + Start Button
    Loads another scene.

---
+++ MechaSquad_UnityProject/Assets/_GAME/Scripts/MenuButtons.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Author: Andrew Seba
+/// </summary>
+public class MenuButtons : MonoBehaviour
+{
+    //just does a direct load.
+    public void _LoadSceneName(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
