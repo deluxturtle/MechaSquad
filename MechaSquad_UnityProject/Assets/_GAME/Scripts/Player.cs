2e55958859f954a17c1a477ba1587843bd11910a
Andrew Seba <sebaturtle@gmail.com>
2019-08-30 18:18:20 -0500

    Letting Go Triggers a fire debug.
    :)

--- MechaSquad_UnityProject/Assets/_GAME/Scripts/Player.cs
+++ MechaSquad_UnityProject/Assets/_GAME/Scripts/Player.cs
@@ -13,6 +13,14 @@ public class Player : MonoBehaviour
     [Tooltip("Shows information about what the player is doing.")]
     public bool printDebug = false;
     GameObject selectedMech = null;
+    float height;
+    float width;
+
+    private void Awake()
+    {
+        width = Screen.width / 2.0f;
+        height = Screen.height / 2.0f;
+    }
 
     public void Init(string name)
     {
@@ -24,6 +32,9 @@ public class Player : MonoBehaviour
         if(Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
+
+            Vector2 startPos = touch.position;
+
             Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
             RaycastHit hit;
             Physics.Raycast(ray, out hit);
@@ -34,6 +45,18 @@ public class Player : MonoBehaviour
                 {
                     selectedMech = clickedObj.gameObject;
                 }
+            }
+
+            if(touch.phase == TouchPhase.Moved)
+            {
+                Vector2 pos = touch.position;
+                //pos.x = (pos.x - width) / width;
+                //pos.y = (pos.y - height) / height;
+            }
+
+            if(touch.phase == TouchPhase.Ended)
+            {
+                Debug.Log("Fire.");
             }
         }
     }
