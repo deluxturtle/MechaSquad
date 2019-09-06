da018959d4fc4e3111524b3a3f5a4f43d311826d
Andrew Seba <sebaturtle@gmail.com>
2019-09-06 09:52:56 -0500

    Raycast from mech
    Need to use mech to shoot instead of using the player script to start it all.

--- MechaSquad_UnityProject/Assets/_GAME/Scripts/Player.cs
+++ MechaSquad_UnityProject/Assets/_GAME/Scripts/Player.cs
@@ -9,9 +9,12 @@ using UnityEngine;
 /// </summary>
 public class Player : MonoBehaviour
 {
-
+    [Tooltip("Testing Explosion Animation Obj")]
+    public GameObject explosion;
     [Tooltip("Shows information about what the player is doing.")]
     public bool printDebug = false;
+    Vector2 startPos;
+    Vector3 shootDir;
     GameObject selectedMech = null;
     float height;
     float width;
@@ -29,40 +32,68 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
+        //Touch Input
         if(Input.touchCount > 0)
         {
+            //touch is original touch (0)
             Touch touch = Input.GetTouch(0);
 
-            Vector2 startPos = touch.position;
-
-            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
-            RaycastHit hit;
-            Physics.Raycast(ray, out hit);
-            Transform clickedObj = hit.collider.transform.parent;
-            if(clickedObj.GetComponent<Mech>() != null)
+            //Grab the starting position of touch and do first contact actions.
+            if (touch.phase == TouchPhase.Began)
             {
-                if(ClickedDifferentMech(clickedObj.gameObject))
+                startPos = touch.position;
+
+                //Ray cast down to mech obj.
+                Ray ray = Camera.main.ScreenPointToRay(touch.position);
+                RaycastHit hit;
+                Physics.Raycast(ray, out hit);
+
+                //Select Mech
+                Transform clickedObj = hit.collider.transform.parent;
+                if (clickedObj.GetComponent<Mech>() != null)
                 {
-                    selectedMech = clickedObj.gameObject;
+                    if (ClickedDifferentMech(clickedObj.gameObject))
+                    {
+                        selectedMech = clickedObj.gameObject;
+                    }
                 }
             }
 
-            if(touch.phase == TouchPhase.Moved)
+            //If dragging shoot bullet with the behavior of a slingshot.
+            if (touch.phase == TouchPhase.Moved)
             {
                 Vector2 pos = touch.position;
-                //pos.x = (pos.x - width) / width;
-                //pos.y = (pos.y - height) / height;
+                Vector2 dist = pos - startPos;
+                shootDir = new Vector3(-dist.x, 0, -dist.y);
+                
+                Debug.DrawRay(selectedMech.transform.position, shootDir.normalized);
             }
 
             if(touch.phase == TouchPhase.Ended)
             {
-                Debug.Log("Fire.");
+                Shoot(shootDir);
             }
         }
+
     }
 
-    void Shoot()
+    /// <summary>
+    /// HitScan shooting style.
+    /// </summary>
+    /// <param name="pDir">Direction of Ray</param>
+    void Shoot(Vector3 pDir)
     {
+        /**NOTE:
+         * Everything works except I think its hitting the collider in the mech
+         * Might need to let the mech handle its own firing at this point.
+         */
+        Ray ray = new Ray(selectedMech.transform.position, pDir.normalized);
+        Debug.DrawRay(selectedMech.transform.position, ray.direction, Color.red, 1f);
+        RaycastHit hit;
+        Physics.Raycast(ray, out hit, 100f);
+        GameObject temp = Instantiate(explosion);
+        Debug.Log(hit.point);
+        temp.transform.position = hit.point;
 
     }
 
