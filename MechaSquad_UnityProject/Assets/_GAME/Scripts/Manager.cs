ebf653f0d253031b2068b955994c90e211739d8b
Andrew Seba <sebaturtle@gmail.com>
2019-09-13 17:51:27 -0500

    Starting Turn Based Sprint
    Added Kenney Fonts
    Created new scene for 2p "shared screen" experience.

--- MechaSquad_UnityProject/Assets/_GAME/Scripts/Manager.cs
+++ MechaSquad_UnityProject/Assets/_GAME/Scripts/Manager.cs
@@ -6,20 +6,20 @@ public class Manager : MonoBehaviour
 {
     [Tooltip("Base Player Prefab to spawn with.")]
     GameObject playerPrefab;
+    List<Player> players = new List<Player>();
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Spawning Players.");
+        SpawnPlayer("Player1");
+        SpawnPlayer("Player2");
     }
 
-    void SpawnPlayer()
+    void SpawnPlayer(string playerName)
     {
-
+        GameObject newPlayer = Instantiate(playerPrefab);
+        players.Add(newPlayer.GetComponent<Player>());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
