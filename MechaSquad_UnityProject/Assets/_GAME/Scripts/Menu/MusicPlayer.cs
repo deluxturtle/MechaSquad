9b067aeb70f1b250cc578ce02ea1779cd5c1f037
Andrew Seba <sebaturtle@gmail.com>
2019-10-04 19:44:31 -0500

    Added some music to the main menu.

---
+++ MechaSquad_UnityProject/Assets/_GAME/Scripts/Menu/MusicPlayer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Author: Andrew Seba
+/// Description: Helps play and change music.
+/// </summary>
+public class MusicPlayer : MonoBehaviour
+{
+    [Tooltip("Place whatever song you want in this spot to listen first.")]
+    public AudioClip startSong;
+    private AudioSource audioSrc;
+
+    private void Start()
+    {
+        audioSrc = GetComponent<AudioSource>();
+        if(audioSrc == null)
+        {
+            audioSrc = gameObject.AddComponent<AudioSource>();
+        }
+
+        if(startSong != null)
+        {
+            audioSrc.clip = startSong;
+            audioSrc.Play();
+        }
+    }
+
+    public static void PlaySong(AudioClip audio)
+    {
+
+    }
+}
